de6ba0d4c4289824c3614bec360851bb1b45ed33
SpencerPerez <spereza2@miumg.edu.gt>
2019-04-18 01:07:37 -0500

    Cliente Controller

--- laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/controller/ClientController.java
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/controller/ClientController.java
@@ -4,9 +4,7 @@ import gt.edu.umg.ingenieria.sistemas.laboratorio1.model.Client;
 import gt.edu.umg.ingenieria.sistemas.laboratorio1.service.ClientService;
 import gt.edu.umg.ingenieria.sistemas.laboratorio1.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +16,57 @@ import java.util.List;
 @RequestMapping("/clientes")
 public class ClientController {
 
-    private final ReportService reportService;
-    private final ClientService clientService;
+    private final ReportService _reportService;
+    private final ClientService _clientService;
 
     @Autowired
-    private ReportService reportService;
-    
+    public ClientController(ReportService reportService, ClientService clientService) {
+        this._reportService = reportService;
+        this._clientService = clientService;
+    }
+
+    @GetMapping("/getById")
+    public Client getById(@RequestParam(name = "id") long id) {
+        return this._clientService.getClienteId(id);
+    }
+
+    @GetMapping("/buscarPorNit")
+    public Client getByNit(@RequestParam(name = "nit") String nit) {
+        return this._clientService.getNitCliente(nit);
+    }
+
+    @GetMapping("/buscarPorNombreApellido")
+    public List<Client> getByNameAndLastName(@RequestParam(name = "query") String nameAndLastName) {
+        return this._clientService.getClienteNombreApellido(nameAndLastName);
+    }
+
+    @GetMapping("/buscarTodos")
+    private List<Client> getByAll() {
+        return this._clientService.getTodosClientes();
+    }
+
+    @PostMapping("/crearCliente")
+    public Object create(@RequestBody Client client) {
+        return this._clientService.crearCliente(client);
+    }
+
+    @PutMapping("/editarCliente/{id}/{nit}")
+    public Client updateNit(@PathVariable long id, @PathVariable String nit) {
+        Client client = getById(id);
+        client.setNit(nit);
+        return this._clientService.registrarCliente(client);
+    }
+
+    @PutMapping("/editarCliente/{id}/{name}/{lastName}")
+    public Client updateNameAndLastName(@PathVariable long id, @PathVariable String name, @PathVariable String lastName) {
+        Client client = getById(id);
+        client.setFirstName(name);
+        client.setLastName(lastName);
+        return this._clientService.registrarCliente(client);
+    }
+
+    @GetMapping("/generarReporteClientes")
+    public String generateReport() {
+        return this._reportService.generarInforme();
+    }
 }
