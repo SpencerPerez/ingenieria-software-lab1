5c7d73c12a6d9189b68b8bcc0f26814a25c061a7
SpencerPerez <spereza2@miumg.edu.gt>
2019-04-19 16:09:50 -0500

    Add .jar

--- laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/Laboratorio1Application.java
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/Laboratorio1Application.java
@@ -8,7 +8,6 @@ public class Laboratorio1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Laboratorio1Application.class, args);
-		System.out.println("se agre el campo_\"birth\"  en la clase \"Client\"");
 	}
 
 }
